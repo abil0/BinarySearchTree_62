920b30cc80e222bd2dbd7108f5e00aeda6f08ea8
abil0 <hoenabil6@gmail.com>
2025-06-26 19:00:40 +0700

    tahap 12

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -143,5 +143,25 @@ void postorder(node *ptr)
     postorder (ptr->leftchild);
     postorder (ptr->rightchild);
     cout << ptr->info << " "; //parent
+
 }
+
+
+};
+
+int main()
+{
+    BinaryTree x;
+    while (true)
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Implement insert operation" << endl;
+        cout << "2. Perform inorder traversal" << endl;
+        cout << "3. Perform preorder traversal" << endl;
+        cout << "4. Perform postorder traversal" << endl;
+        cout << "5. Exit" << endl;
+        cout << "\nEnter your choice (1-5) : ";
+
+  
+    }
 }
