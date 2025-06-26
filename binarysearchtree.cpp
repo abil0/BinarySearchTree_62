9e638fd9bf07c406e6f28ddc12e0bb041a8b8eb3
abil0 <hoenabil6@gmail.com>
2025-06-26 18:11:43 +0700

    tahap 3

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -7,5 +7,11 @@ public:
      int info;
      node*leftchild;
      node*rightchild;
-     
-}
+
+     //constuctor for the node class
+     node()
+     {
+        leftchild = nullptr; //initialize left child to null
+        rightchild = nullptr; //initialize right child to null
+     }
+};
